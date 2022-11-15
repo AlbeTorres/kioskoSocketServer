4aa804139282099431642452555e97cc49f8fdcd
albetorres <albertocorreoficial@gmail.com>
2022-11-15 09:48:31 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const io= require('socket.io')(8900,{
+    cors:{
+        origin: 'http://localhost:5173'
+
+    }
+})
+
+io.on('connection',(socket)=>{
+    console.log('usuario conectado')
+}
+)
