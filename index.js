dba194fa925fbfa822f9da7225373e5ccf3f80ab
albetorres <albertocorreoficial@gmail.com>
2022-11-16 13:47:13 -0500

    reparado server de mierda

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const io= require('socket.io')(8900,{
 })
 
 io.on('connection',(socket)=>{
-    console.log('usuario conectado')
+    //usuario conectado
+    console.log('usuario conectado'+socket.id)
+
+    socket.on('producto estado',()=>{
+        io.emit('getproductos','eso')
+    })
+
+    //usuario desconectado
+    socket.on('disconnect',()=>{
+        console.log('usuario desconectado')
+    })
+    
+
 }
 )
+
+io.listen(5173)
